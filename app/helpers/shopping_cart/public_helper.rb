96f39dc1e648bbe913d4955a8e2d83951774dfa4
SarCasm <sarcasm008@gmail.com>
2016-03-07 18:31:28 +0200

    ver 15

--- app/helpers/shopping_cart/public_helper.rb
+++ app/helpers/shopping_cart/public_helper.rb
@@ -1,11 +1,12 @@
 module ShoppingCart::PublicHelper
-  def add_cart_button(product, classes: ['btn', 'btn-primary'], qnt: true, number_class: 'form-control')
+  def add_cart_button(product, text: "Add to Cart", classes: ['btn', 'btn-primary'], qnt: true, number_class: 'form-control')
     render 'shopping_carts/add',
       locals: {
         product: product,
         classes: classes,
         qnt: qnt,
         number_class: number_class
+        text: text
       }
   end
 end
