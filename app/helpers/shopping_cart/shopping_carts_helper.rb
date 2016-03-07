64fa864d6f127055db5e08d64b282fa8820553dd
SarCasm <sarcasm008@gmail.com>
2016-03-07 16:09:34 +0200

    yeah

--- app/helpers/shopping_cart/shopping_carts_helper.rb
+++ app/helpers/shopping_cart/shopping_carts_helper.rb
@@ -1,4 +1,4 @@
-module ShoppingCartsHelper
+module ShoppingCart::ShoppingCartsHelper
   def price_without_discount_if_any(obj)
     if obj.sum != obj.sum_without_discount
       number_to_currency obj.sum_without_discount
