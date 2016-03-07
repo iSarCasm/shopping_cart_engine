15ccab7f6ec8acec7862c6d1cbf10d154f306b40
SarCasm <sarcasm008@gmail.com>
2016-03-07 17:28:11 +0200

    lol

--- app/helpers/shopping_cart/application_helper.rb
+++ app/helpers/shopping_cart/application_helper.rb
@@ -1,13 +1,4 @@
 module ShoppingCart
   module ApplicationHelper
-    def add_cart_button(product, classes: ['btn', 'btn-primary'], qnt: true, number_class: 'form-control')
-      render 'shopping_carts/add',
-        locals: {
-          product: product,
-          classes: classes,
-          qnt: qnt,
-          number_class: number_class
-        }
-    end
   end
 end
--- lib/shopping_cart/engine.rb
+++ lib/shopping_cart/engine.rb
@@ -1,5 +1,11 @@
 module ShoppingCart
   class Engine < ::Rails::Engine
+    initializer 'shopping_cart.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        helper ShoppingCart::PublicHelper
+      end
+    end
+
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
