2dcb87a64f1da1a0c0d9b12d3a28cf6d1f8b844e
SarCasm <sarcasm008@gmail.com>
2016-03-07 18:38:46 +0200

    sasafsa

--- app/helpers/shopping_cart/public_helper.rb
+++ app/helpers/shopping_cart/public_helper.rb
@@ -1,7 +1,6 @@
 module ShoppingCart::PublicHelper
   def add_cart_button(product, text: "Add to Cart", classes: ['btn', 'btn-primary'], qnt: true, number_class: 'form-control')
-    render 'shopping_carts/add',
-      locals: {
+    render 'shopping_carts/add', locals: {
         product: product,
         classes: classes,
         qnt: qnt,
