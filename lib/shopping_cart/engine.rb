8c90d782329025fb8a56da67cd2aebc64e1eafe9
SarCasm <sarcasm008@gmail.com>
2016-03-07 17:34:33 +0200

    ver

--- lib/shopping_cart/engine.rb
+++ lib/shopping_cart/engine.rb
@@ -1,10 +1,10 @@
 module ShoppingCart
   class Engine < ::Rails::Engine
-    initializer 'shopping_cart.action_controller' do |app|
-      ActiveSupport.on_load :action_controller do
-        helper ShoppingCart::PublicHelper
-      end
-    end
+    # initializer 'shopping_cart.action_controller' do |app|
+    #   ActiveSupport.on_load :action_controller do
+    #     helper ShoppingCart::PublicHelper
+    #   end
+    # end
 
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
