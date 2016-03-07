895feddd1deecf92ea676f7c9140b27bfb64d072
SarCasm <sarcasm008@gmail.com>
2016-03-07 17:42:40 +0200

    ver 7

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,6 +1,20 @@
 require "shopping_cart/engine"
 
-
-
 module ShoppingCart
+  mattr_accessor :product_class
+  mattr_accessor :coupon_class
+
+  mattr_accessor :with_coupons
+
+  class Engine < Rails::Engine
+    initializer "my_engine.configure_rails_initialization" do |app|
+    end
+  end
+
+  # This is what I was trying to figure out
+  def self.config(&block)
+    @@config ||= MyEngine::Configuration.new
+    yield @@config if block
+    return @@config
+  end
 end
