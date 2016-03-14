99f6ffd668a91bd84cfa7df468944128ddb835f9
SarCasm <sarcasm008@gmail.com>
2016-03-14 17:21:35 +0200

    fast push

--- app/controllers/shopping_cart/application_controller.rb
+++ app/controllers/shopping_cart/application_controller.rb
@@ -1,5 +1,5 @@
 module ShoppingCart
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
     def current_cart
       @_shopping_cart ||= ShoppingCart::Cart.new(session)
     end
