6162bde712ef7aeb6c23588d23ff583a639cac50
SarCasm <sarcasm008@gmail.com>
2016-03-14 17:46:57 +0200

    fast push

--- app/helpers/shopping_cart/application_helper.rb
+++ app/helpers/shopping_cart/application_helper.rb
@@ -1,7 +1,7 @@
 module ShoppingCart
   module ApplicationHelper
     def current_cart
-      @_shopping_cart ||= ShoppingCart::Cart.new(session)
+      @_the_shopping_cart ||= ShoppingCart::Cart.new(session)
     end
   end
 end
