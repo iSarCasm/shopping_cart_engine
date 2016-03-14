353d632fe0b82a6391aef7c94f36e350065a9027
SarCasm <sarcasm008@gmail.com>
2016-03-14 17:07:35 +0200

    fast push

--- lib/shopping_cart/engine.rb
+++ lib/shopping_cart/engine.rb
@@ -2,6 +2,12 @@ module ShoppingCart
   class Engine < ::Rails::Engine
     isolate_namespace ShoppingCart
 
+    initializer 'shopping_cart.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        helper ShoppingCart::CartsHelper
+      end
+    end
+
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
