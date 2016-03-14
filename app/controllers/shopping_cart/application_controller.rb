045e4d68a0778ca68a8e60fd7c4cc23b859cb701
SarCasm <sarcasm008@gmail.com>
2016-03-14 22:08:09 +0200

    fast push

--- app/controllers/shopping_cart/application_controller.rb
+++ app/controllers/shopping_cart/application_controller.rb
@@ -1,5 +1,7 @@
 module ShoppingCart
   class ApplicationController < ::ApplicationController
+    include MyApp::Engine.routes.url_helpers
+
     def current_cart
       @_shopping_cart ||= ShoppingCart::Cart.new(session)
     end
