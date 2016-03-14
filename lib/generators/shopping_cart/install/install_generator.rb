3693890575b97e1148bf1253713781bc94f03421
SarCasm <sarcasm008@gmail.com>
2016-03-14 16:50:26 +0200

    fast push

---
+++ lib/generators/shopping_cart/install/install_generator.rb
@@ -0,0 +1,15 @@
+require 'rails/generators/base'
+
+module ShoppingCart
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
+
+    def copy_initializer
+      copy_file "initializer.rb", "config/initializers/shopping_cart.rb"
+    end
+
+    def copy_locale
+      copy_file "../../../../../config/locales/en.yml", "config/locales/shopping_cart.en.yml"
+    end
+  end
+end
---
+++ lib/generators/shopping_cart/install/templates/initializer.rb
@@ -0,0 +1,5 @@
+ShoppingCart.setup do |config|
+  config.product_class  = 'Product'
+  config.coupon_class   = 'Coupon'
+  config.with_coupons   = true
+end
